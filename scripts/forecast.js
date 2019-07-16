fc14acfb089119a5c8b6290ab35a833fc192722f
Rogerio <rogerio_contato@hotmail.com>
2019-07-16 08:01:24 -0300

    i added some commits

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,20 +1,23 @@
 const key = 'F5RMMBmtxVA7CduMa80tIWmzJFwDNbDJ'
 
+// all asynchronous function return a promise
 const getCity = async (city) => {
+
     const base = 'http://dataservice.accuweather.com/locations/v1/cities/search'
     const query = `?apikey=${key}&q=${city}`
 
+    // await promise to be resolve
     const response = await fetch(base + query)
 
     if(response.status !== 200) {
-        throw new Error('cannot fetch the data')
+        throw new Error('Cannot fetch the data')
     }
-
+    // response.json return a promise
     const data = await response.json()
-
     return data[0]
+
 }
 
-getCity("rio de janeiro")
+getCity('são paulo')
     .then(data => console.log(data))
     .catch(err => console.log(err))
